43bde343aae75908515bd6a7533c686bc1689738
Siva M <Muniappan.Sivakumar@gmail.com>
2020-07-26 00:27:15 -0500

    Initial Commit

---
+++ CodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeScanner
+//
+//  Created by Sivakumar Muniappan on 7/26/20.
+//  Copyright © 2020 Taby. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
